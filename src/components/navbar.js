d45c8b0334942dc71ba95f4fd3b6895410f3df11
carlxw <87335263+carlxw@users.noreply.github.com>
2023-01-19 21:08:18 -0500

    Upload files

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Carl Wang</h1>
+            <div className="link_pages">
+                <a href="/">Home</a>
+                <a href="/my-story">My Story</a>
+                <a href="/experiences">Experiences</a>
+                <a href="/personal-projects">Personal Projects</a>
+                <a href="/contact">Contact/Socials</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
