167a7292e61d822d9436ac416200446bacb9181a
carlxw <87335263+carlxw@users.noreply.github.com>
2023-01-29 14:54:30 -0500

    Add links to personal project

--- src/components/ProjCard.js
+++ src/components/ProjCard.js
@@ -6,6 +6,14 @@ const ProjCard = ({ data }) => {
                 <h2 id={ data[i].title }>{ data[i].title }</h2>
                 <img style={ { height: 100, width: 100 } }src={ require(`../data/img/${data[i].img}`) } alt="logo" />
                 <p>{ data[i].desc }</p>
+                {   
+                    // If there is a Github link
+                    data[i].github && <a href={ data[i].github } target="_blank" rel="noreferrer">Github</a>
+                }
+                {   
+                    // If there is a Devpost link
+                    data[i].devpost && <a href={ data[i].devpost } target="_blank" rel="noreferrer">DevPost</a>
+                }
             </div>
         );
     }
