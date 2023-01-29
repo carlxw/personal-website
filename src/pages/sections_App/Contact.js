afbd4954af5ae3170b47a1dcfad919201b476b5b
carlxw <87335263+carlxw@users.noreply.github.com>
2023-01-29 00:32:01 -0500

    Open social links in a new tab

--- src/pages/sections_App/Contact.js
+++ src/pages/sections_App/Contact.js
@@ -2,11 +2,11 @@ const Contact = () => {
     return (
         <div className="contact" id="contact">
             <h1 id="contact">Contact</h1>
-            <a href="https://www.linkedin.com/in/carlxwang/">LinkedIn</a> 
+            <a href="https://www.linkedin.com/in/carlxwang/" target="_blank" rel="noreferrer">LinkedIn</a> 
             <br />
-            <a href="https://github.com/carlxw">GitHub</a>
+            <a href="https://github.com/carlxw" target="_blank" rel="noreferrer">GitHub</a>
             <br />
-            <a href="mailto:someone@yoursite.com">Email</a>
+            <a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer">Email</a>
         </div>
     );
 
