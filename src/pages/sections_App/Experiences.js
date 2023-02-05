72b1b265b1aa18ad51f36dea32ce95985c80bc4f
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-04 23:15:06 -0500

    Start Experiences

--- src/pages/sections_App/Experiences.js
+++ src/pages/sections_App/Experiences.js
@@ -1,5 +1,6 @@
 import WorkExpCard from "../../components/WorkExpCard";
 
+import "./css/Experiences.css";
 
 const jsonArr = require("../../data/json/work_experience.json")
 
@@ -7,7 +8,7 @@ const Experiences = () => {
     return (
         <section id="experiences">
             <div className="experiences">
-                <h1>Experiences -------------------------</h1>
+                <h1>Experiences</h1>
                 <p>This is a visual tree that gives a good idea of my work experience</p>
                 <WorkExpCard data={ jsonArr } />
             </div>
