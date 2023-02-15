6127109a3d6d7c0c0ea12e936d6023f1b1d1108e
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-14 22:11:29 -0500

    Push

--- src/components/WorkExpCard.js
+++ src/components/WorkExpCard.js
@@ -1,8 +1,10 @@
+import "./css/WorkExpCard.css";
+
 const WorkExpCard = ({ data }) => {
     const render = [];
     for (var i = 0; i < data.length; i++) {
         render.push(
-            <div key={ data[i].pos_title }>
+            <div key={ data[i].pos_title } className="workexp_card">
                 <h2 id={ data[i].pos_title }>{ data[i].pos_title }</h2>
                 <p>{ data[i].company }</p>
                 <p>{ data[i].duration }</p>
--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -5,14 +5,19 @@ const jsonArr = require("../data/json/work_experience.json")
 
 const Experiences = () => {
     return (
-        <div className="experiences" id="experiences">
-            <h1>Experiences -------------------------</h1>
-            <p>This is a visual tree that gives a good idea of my work experience</p>
-            <WorkExpCard data={ jsonArr } />
+        <div id="margins">
+            <div className="experiences" id="experiences">
+                <h1 id="h1_title">Experiences</h1>
+                <p>This is a visual tree that gives a good idea of my work experience</p>
 
-            <Link to="/personal_projects" className="navlinks" id="next_page">Personal Projects {">"}</Link>
-            <Link to="/skills" className="navlinks" id="prev_page">{"<"} Skills</Link>
-            <div className="cls" />
+                <div className="experiences_content">
+                    <WorkExpCard data={ jsonArr } />
+                </div>
+            
+                <Link to="/personal_projects" className="navlinks" id="next_page">Personal Projects {">"}</Link>
+                <Link to="/skills" className="navlinks" id="prev_page">{"<"} Skills</Link>
+                <div className="cls" />
+            </div>
         </div>
     );
 }
