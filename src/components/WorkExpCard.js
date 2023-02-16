50082fce9da12246b3cbbbd132b0615a9f32584f
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-16 16:48:09 -0500

    Implement bullet points

--- src/components/WorkExpCard.js
+++ src/components/WorkExpCard.js
@@ -3,12 +3,21 @@ import "./css/WorkExpCard.css";
 const WorkExpCard = ({ data }) => {
     const render = [];
     for (var i = 0; i < data.length; i++) {
+        // Extracts the description
+        const descArr = data[i].desc.map(x => (
+            // Key will raise problems if I use the same description over and over?
+            <li className="list" style={{ "color": "black" }} key={ x }>{ x }</li>
+        ));
+
         render.push(
             <div key={ data[i].pos_title } className="workexp_card">
                 <div className="left_info">
                     <h2 id={ data[i].pos_title }>{ data[i].pos_title }</h2>
                     <p id="company_duration">{ `${data[i].company} (${data[i].duration})` }</p>
-                    <p>{ data[i].desc }</p>
+                    
+                    <ul>
+                        { descArr }
+                    </ul>
                 </div>
 
                 <div className="right_img">
