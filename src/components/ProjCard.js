3a4cd852cc88e9d0a320a17805459812fb1df29c
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-18 16:14:53 -0500

    Styled personal projects

--- src/components/ProjCard.js
+++ src/components/ProjCard.js
@@ -1,25 +1,60 @@
+import "./css/ProjCard.css";
+
 const ProjCard = ({ data }) => {
     const render = [];
     for (var i = 0; i < data.length; i++) {
+        // Extract the tags and list-description
+        const tags = data[i].tags.map((x) => (
+            x.link ? <a className="bubble" href={ x.link } target="_blank" rel="noreferrer">{ x.title }</a> : <p className="bubble" id="nolink"> { x.title } </p>
+        ));
+
+        const desc = data[i].desc2.map((x) => (
+            // Key will raise problems if I use the same description over and over?
+            <li className="list" style={{ "color": "black" }} key={ x }>{ x }</li>
+        ));
+
         render.push(
-            <div key={ data[i].title }>
-                <h2 id={ data[i].title }>{ data[i].title }</h2>
-                <img style={ { height: 100, width: 100 } }src={ require(`../data/img/${data[i].img}`) } alt="logo" />
-                <p>{ data[i].desc }</p>
-                {   
-                    // If there is a Github link
-                    data[i].github && <a href={ data[i].github } target="_blank" rel="noreferrer">Github</a>
-                }
-                {   
-                    // If there is a Devpost link
-                    data[i].devpost && <a href={ data[i].devpost } target="_blank" rel="noreferrer">DevPost</a>
-                }
+            <div className="proj_card" key={ data[i].title }>
+                <div className="proj_cardcontent">
+                    <div className="proj_left">
+                        <h2 id={ data[i].title }>{ data[i].title }</h2>
+                        <p>{ data[i].desc1 }</p>
+
+                        <ul>
+                            { desc }
+                        </ul>
+                    </div>
+                    
+                    <div className="proj_right">
+                        <img src={ require(`../data/img/${data[i].img}`) } alt="logo" />
+                    </div>
+                    <div className="cls" />
+                </div>
+
+                <div className="proj_footer">
+                    <div className="links">
+                        {   
+                            // If there is a Github link
+                            data[i].github && <a className="bubble" href={ data[i].github } target="_blank" rel="noreferrer">Github</a>
+                        }
+                        {   
+                            // If there is a Devpost link
+                            data[i].devpost && <a className="bubble" href={ data[i].devpost } target="_blank" rel="noreferrer">DevPost</a>
+                        }
+                    </div>
+
+                    <div className="tags">
+                        {
+                            data[i].tags && tags
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
 
     return (
-        <div className="proj_card">
+        <div>
             {/* This renders an array */}
             { render }
         </div>
--- src/pages/PersonalProjects.js
+++ src/pages/PersonalProjects.js
@@ -5,14 +5,18 @@ const jsonArr = require("../data/json/personal_projects.json");
 
 const PersonalProjects = () => {
     return (
-        <div className="personal-projects" id="personal_projects">
-            <h1>Personal Projects -------------------------</h1>
-            <p>A collection of things that I made or am apart of</p>
-            <ProjCard data={jsonArr} />
+        <div id="margins">
+            <div className="personal-projects" id="personal_projects">
+                <h1 id="h1_title">Personal Projects</h1>
 
-            <Link to="/contact" className="navlinks" id="next_page">Contact/Socials {">"}</Link>
-            <Link to="/work_experience" className="navlinks" id="prev_page">{"<"} Work Experiences</Link>
-            <div className="cls" />
+                <div className="proj_content">
+                    <ProjCard data={jsonArr} />
+                </div>
+
+                <Link to="/contact" className="navlinks" id="next_page">Contact/Socials {">"}</Link>
+                <Link to="/work_experiences" className="navlinks" id="prev_page">{"<"} Work Experiences</Link>
+                <div className="cls" />
+            </div>
         </div>
     );
 
