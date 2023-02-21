7a31d7859166c915dd78e5f4b8c771f5b90a4913
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-21 11:50:29 -0500

    Implement hash router

--- src/App.js
+++ src/App.js
@@ -15,29 +15,29 @@ import "./css/App.css";
 import "./css/Tooltip.css";
 
 // Objects
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { BrowserView, MobileView } from "react-device-detect";
 
 function App() {
     return (
         <div>
             <BrowserView>
-                <BrowserRouter>
+                <HashRouter>
                     <div className="app">
                         <Navbar />
-                            <div className="content">
-                                <Routes>
-                                    <Route path="/" element={<Hello />} />
-                                    <Route path="/personal-website" element={<Hello />} />
-                                    <Route path="/skills" element={<Skills />} />
-                                    <Route path="/work_experiences" element={<WorkExperiences />} />
-                                    <Route path="/personal_projects" element={<PersonalProjects />} />
-                                    <Route path="/contact" element={<Contact />} />
-                                    <Route path="*" element={<Error />} />
-                                </Routes>
-                            </div>
+                        <div className="content">
+                            <Routes>
+                                <Route path="/" element={<Hello />} />
+                                <Route path="/personal-website" element={<Hello />} />
+                                <Route path="/skills" element={<Skills />} />
+                                <Route path="/work_experiences" element={<WorkExperiences />} />
+                                <Route path="/personal_projects" element={<PersonalProjects />} />
+                                <Route path="/contact" element={<Contact />} />
+                                <Route path="*" element={<Error />} />
+                            </Routes>
+                        </div>
                     </div>
-                </BrowserRouter>
+                </HashRouter>
             </BrowserView>
             <MobileView>
                 <Mobile />
