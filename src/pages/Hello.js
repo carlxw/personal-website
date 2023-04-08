f34f2b079eea6fd8504460452123bc9192662d45
carlxw <87335263+carlxw@users.noreply.github.com>
2023-04-07 20:19:57 -0400

    Fix alignment, remove redundant code

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -1,50 +1,17 @@
 import image from "../data/img/cartoonme.png";
-// import { Link } from "react-router-dom";
-// import Typewriter from "typewriter-effect";
 
 import config from "../data/json/text.json";
 
 const Hello = () => {   
-    // const strArr = [
-    //     "a programmer.",
-    //     "ambitious.",
-    //     "creative.",
-    //     "determined.",
-    //     "hard working.",
-    //     "a quick learner.",
-    //     "versatile.",
-    //     "curious.",
-    //     "a Google master."
-    // ];
-
     const date = new Date();
     const welcome_msg1 = config.welcome1.replace("{ INSERT_DATE }", date.getFullYear() - 2004);
     const welcome_msg2 = config.welcome2;
 
     return (
         <div>
-            <div className="hello flex-row flex-center">
+            <div className="hello flex-row">
                 <div className="hello-left flex-col flex-left">
                     <h1>{ config.welcomeh1 }</h1>
-                    {/* <Typewriter 
-                        id="tw2"
-                        style={{
-                            "display": "block",
-                            "font-size": "2em",
-                            "margin-top": "0.67em",
-                            "margin-bottom": "0.67em",
-                            "margin-left": "0",
-                            "margin-right": "0",
-                            "font-weight": "bold",
-                        }}
-                        options={{
-                            autoStart: true,
-                            strings: strArr,
-                            loop: true,
-                            cursor: "",
-                            skipAddStyles: false,
-                        }}
-                    /> */}
 
                     <div className="hello-txt flex-col flex-left">
                         <p>{ welcome_msg1 }</p>
@@ -52,9 +19,7 @@ const Hello = () => {
                     </div>
                 </div>
 
-                <div className="hello-right">
-                    <img src={image} alt="A cartoon of me" />
-                </div>
+                <img className="hello-right" src={image} alt="A cartoon of me" />
 
                 {/* <Link to="/skills" className="navlinks" id="next-page">{ ">" }</Link> */}
             </div>
