470af3c1be23df3e444c22d8674cb71926740f5f
carlxw <87335263+carlxw@users.noreply.github.com>
2023-04-08 22:50:05 -0400

    Implement hidden message

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -8,15 +8,16 @@ const Hello = () => {
     const welcome_msg2 = config.welcome2;
 
     return (
-        <div>
+        <div className="hello-container flex-col">
             <div className="hello flex-row">
                 <div className="hello-left flex-col flex-left">
-                    <h1>{ config.welcomeh1 }</h1>
+                    <h1 className="hello-h1">{ config.welcomeh1 }</h1>
 
                     <div className="hello-txt flex-col flex-left">
-                        <p>{ welcome_msg1 }</p>
+                        <p id="abc">{ welcome_msg1 }</p>
                         <p>{ welcome_msg2 }</p>
                     </div>
+                    <p id="hello-msg">{ config.welcome_msg }</p>
                 </div>
 
                 <img className="hello-right" src={image} alt="A cartoon of me" />
