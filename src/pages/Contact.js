8740ee6ddc7e3896d92cac6e9fe9e4c100376731
carlxw <87335263+carlxw@users.noreply.github.com>
2023-05-05 12:11:13 -0400

    Push

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import { MdEmail } from "react-icons/md";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Contact = () => {
-    // Fix <a> not opening immediately
+    // Fix <a> not opening immediately, but could've been my own PC. At least this hides the link
     const openURL = (url) => {
         const tab = window.open(url, "_blank", "noopener, noreferrer");
         if (tab) tab.opener = null;
@@ -16,8 +16,8 @@ const Contact = () => {
                 <p>Thanks for checking out my website! If you want to connect, feel free to use any of the options below!</p>
 
                 <div className="contact-links flex-row">
-                    <div onClick={() => openURL("https://www.linkedin.com/in/carlxwang/")}className="contact-btn flex-row">
-                        <a >LinkedIn</a> <AiFillLinkedin className="icon" />
+                    <div onClick={() => openURL("https://www.linkedin.com/in/carlxwang/")} className="contact-btn flex-row">
+                        <a>LinkedIn</a> <AiFillLinkedin className="icon" />
                     </div>
 
                     <div onClick={() => openURL("https://github.com/carlxw")} className="contact-btn flex-row">
@@ -29,7 +29,7 @@ const Contact = () => {
                     </div>
                 </div>
 
-                <p id="disclaimer">I do not own at least 70% of the images used on this website. Soooooo I envoke Copyright Disclaimer under section 107 of the Copyright Act of 1976.<br />Email me if you feel some empowerment motivated by copyrightnanigans.</p>
+                <p id="disclaimer">I probably do not own at least 30%-50% of the images used on this website. Soooooo I envoke Copyright Disclaimer under section 107 of the Copyright Act of 1976.<br />Email me if you feel some empowerment motivated by copyrightnanigans.</p>
             </div>
         </div>
     );
