74e4a585677eeb2a8c8b22ba2cbfe00debd09784
carlxw <87335263+carlxw@users.noreply.github.com>
2023-08-27 13:25:01 -0400

    Style overlay

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,3 +1,7 @@
+import { AiFillGithub, AiFillCloseCircle } from "react-icons/ai";
+import { SiDevpost } from "react-icons/si";
+import { FiExternalLink } from "react-icons/fi";
+
 const off = () => {
     document.getElementById("overlay").style.display = "none";
 }
@@ -12,23 +16,36 @@ const Overlay = ({ data }) => {
         border: "none"
     };
 
+    // Extract the tags and list-description
+    const tags = data?.tags?.map((x, idx) => (
+        <a href={ x.link } target="_blank" rel="noreferrer" key={ `${idx}_tags` }>{ x.title }<FiExternalLink id="ext_link_icon" className="icon" /></a> 
+    ));
+
+    const desc = data?.desc_points?.map((x, idx) => (
+        <li className="points_list" style={{ "color": "black" }} key={ `${idx}_desc` }>{ x }</li>
+    ));
+
     if (Object.keys(data).length !== 0) return (
-        <div id="overlay" onClick={ off }>
-            <div className="overlay_header">
-                <h1>{ data?.title }</h1>
-                <h2 style={{ fontStyle: "italic" }}>{ data?.header }</h2>
-                <img alt="Project logo" src={ require(`../data/img/${ data?.img }`) }/>
+        <div id="overlay">
+            <div className="overlay_upper flex-row">
+                <div className="overlay_left flex-col">
+                    <h1 id="proj_title">{ data?.title }</h1>
+                    <p id="proj_desc">{ data?.header }</p>
+                    <hr id="overlay_hr" /> 
+                    <p id="proj_story">{ `"${ data?.story }"` }</p>
+                    <ul>{ desc }</ul>
+                </div>
+                
+                <img src={ require(`../data/img/${ data.img }`) } alt="Project logo" />
             </div>
-            
-            <hr style={ hr_style } />
 
-            <div className="overlay_body">
-                <p>{ data?.story }</p>
-                <ul>{ data?.desc_points?.map((x, idx) => <li key={ `${idx}_bullet_points` }>{ x }</li>) }</ul>
+            <div className="overlay_footer flex-row">
+                <p>Tags:</p>
+                { data?.tags && tags }
             </div>
 
-            <div className="overlay_footer">
-            { data?.tags?.map((x, idx) => <a href={ x.link } key={ `${idx}_bullet_points` }>{ x.title }</a>) }
+            <div className="close_button" onClick={ off }>
+                <AiFillCloseCircle  size={ 30 } />
             </div>
         </div>
     );
