55f03c345cc35aa80a478526dcbff458e934a788
carlxw <87335263+carlxw@users.noreply.github.com>
2023-08-28 18:07:00 -0400

    Implement overlay dim and reposition elements

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,21 +1,16 @@
-import { AiFillGithub, AiFillCloseCircle } from "react-icons/ai";
-import { SiDevpost } from "react-icons/si";
 import { FiExternalLink } from "react-icons/fi";
 
 const off = () => {
     document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay_dim").style.display = "none";
 }
 
 const on = () => {
     document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay_dim").style.display = "block";
 }
 
 const Overlay = ({ data }) => { 
-    const hr_style = {
-        backgroundColor: "white",
-        border: "none"
-    };
-
     // Extract the tags and list-description
     const tags = data?.tags?.map((x, idx) => (
         <a href={ x.link } target="_blank" rel="noreferrer" key={ `${idx}_tags` }>{ x.title }<FiExternalLink id="ext_link_icon" className="icon" /></a> 
@@ -26,28 +21,34 @@ const Overlay = ({ data }) => {
     ));
 
     if (Object.keys(data).length !== 0) return (
-        <div id="overlay">
-            <div className="overlay_upper flex-row">
-                <div className="overlay_left flex-col">
-                    <h1 id="proj_title">{ data?.title }</h1>
-                    <p id="proj_desc">{ data?.header }</p>
-                    <hr id="overlay_hr" /> 
-                    <p id="proj_story">{ `"${ data?.story }"` }</p>
-                    <ul>{ desc }</ul>
+        <>
+            <div id="overlay">
+                <div className="overlay_upper flex-row" onClick={ off }>
+                    <div className="overlay_left flex-col">
+                        <h1 id="proj_title">{ data?.title }</h1>
+                        <p id="proj_desc">{ data?.header }</p>
+                        <hr id="overlay_hr" /> 
+                        <p id="proj_story">{ `"${ data?.story }"` }</p>
+                        <ul>{ desc }</ul>
+                    </div>
+                    
+                    <img src={ require(`../data/img/${ data?.img }`) } alt="Project logo" />
                 </div>
-                
-                <img src={ require(`../data/img/${ data.img }`) } alt="Project logo" />
+
+                <div className="overlay_footer flex-row">
+                    <div className="overlay_tags flex-row">
+                        <p>Tags:</p>
+                        { data?.tags && tags }
+                    </div>
+
+                    <div className="close_button" onClick={ off }>
+                        Close
+                    </div>
+                </div>
             </div>
 
-            <div className="overlay_footer flex-row">
-                <p>Tags:</p>
-                { data?.tags && tags }
-            </div>
-
-            <div className="close_button" onClick={ off }>
-                <AiFillCloseCircle  size={ 30 } />
-            </div>
-        </div>
+            <div id="overlay_dim"/>
+        </>
     );
 }
 
