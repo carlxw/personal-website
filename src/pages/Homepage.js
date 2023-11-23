592985cdea0cf8ff82b4b2df93914d56872ef1e3
carlxw <87335263+carlxw@users.noreply.github.com>
2023-11-23 17:41:07 -0500

    Update the homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,4 @@
 import image from "../data/img/cartoonme.png";
-import text from "../data/json/homepage_text.json";
 
 const Hello = () => {   
     const date = new Date();
@@ -9,7 +8,9 @@ const Hello = () => {
             <div className="homepage_left flex-col">
                 <h1 id="homepage_h1">You get free money</h1>
                 <div id="homepage_text">
-                    { text.map((x, idx) => <p key={ idx }>{ x.replace("{ INSERT_AGE }", date.getFullYear() - 2004) }</p>)}
+                    <p>Just kidding! Now that I have your attention, Hi! My name is Carl and I am a { date.getFullYear() - 2004 } years old student studying Electrical Engineering at the University of Waterloo!</p>
+                    <p>My favourite quote is from Bill Gates: <em>“I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.”</em></p>
+                    <p>If you ever want to connect, feel free to find my socials (doxxing resources) below!</p>
                 </div>
             </div>
             <div className="homepage_right">
--- todo.md
+++ todo.md
@@ -1,11 +1,12 @@
 # Todo
 * Fix footer ❌
     * ~~Make the side buttons show click pointer on hover; fix the display style~~
-    * Add a hover colour to the social media icons?
+    * ~~Add a hover colour to the social media icons?~~
 * Fix title page ❌
     * Remove the text - it is too much
     * Name, position, etc.
     * Implement a theme (changing the background, etc.) (https://www.makeuseof.com/css-background-patterns-examples/)
+    * Add an animation that attracts the user to my personal projects page? (same animation for socials)
 * Personal projects ❌
     * Add YouTube note taker project
     * Add Office Scripts
